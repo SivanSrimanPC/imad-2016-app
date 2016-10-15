d3ad59c9b4ddf647599ea87a3cafec9ca17b313d
P C Sivan Sriman <SivanSrimanPC@users.noreply.github.com>
2016-10-15 20:18:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('article1', function(req,res)
 {
     res.send('Article one is requested');
-});
+
+}
+)
+;
+
 }
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
